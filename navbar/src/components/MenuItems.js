2bfb82d15a4f2de0a64292785bfb5112e64f7248
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-02-25 14:22:22 +0200

    Responsive navbar

---
+++ navbar/src/components/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title: "Home",
+        url: "#",
+        cName: "nav-links"
+    },
+
+    {
+        title: "Services",
+        url: "#",
+        cName: "nav-links"
+    },
+
+    {
+        title: "Products",
+        url: "#",
+        cName: "nav-links"
+    },
+
+    {
+        title: "Contact Us",
+        url: "#",
+        cName: "nav-links"
+    },
+
+    {
+        title: "Sign up",
+        url: "#",
+        cName: "nav-links-mobile"
+    },
+
+]
