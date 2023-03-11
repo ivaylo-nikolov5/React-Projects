574fa2661ac3642ddf2eeee133d7f84c88ead43a
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-03-11 18:38:02 +0200

    Added data gathering from an API and finished the project

--- simple-contact-list/src/App.js
+++ simple-contact-list/src/App.js
@@ -3,9 +3,32 @@ import ContactCard from "./ContactCard";
 import "./general.css";
 
 function App() {
+    const [data, setData] = React.useState([])
+
+    console.log(data);
+    const dataAsContactCards = data.map((result, index) => {
+        return <ContactCard
+                    image={result.picture.large}
+                    firstName={result.name.first}
+                    lastName={result.name.last}
+                    email={result.email}
+                    age={result.dob.age}
+                    key={index}
+                />
+    })
+
+    React.useEffect(() => {
+        fetch("https://randomuser.me/api/?results=1127")
+        .then(response => response.json())
+        .then(data => {
+            setData(data.results);
+        })
+    }, [])
+
+
     return (
-        <div>
-            <ContactCard />
+        <div className="contactsContainer">
+            {dataAsContactCards}
         </div>
     )
 }
--- simple-contact-list/src/ContactCard.jsx
+++ simple-contact-list/src/ContactCard.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import { React, useState } from 'react';
 import "./contactCard.css";
 
-const ContactCard = () => {
+const ContactCard = (props) => {
+    const [ageShown, setAgeShown] = useState(false);
+    const [ageConditionText, setAgeConditionText] = useState("Show");
+
+    function handleClick() {
+        setAgeShown(prevAge => !prevAge);
+        setAgeConditionText(prevText => {
+            return prevText === "Show" ? "Hide" : "Show"; 
+        })
+    }
+
     return (
-        <div>   
-            
+        <div className='contactCardContainer'>   
+            <div>
+                <img src={props.image} alt="" className='contactImage'/>
+            </div>
+
+            <div className='contactInfo'>
+                <p>Name: {props.firstName} {props.lastName}</p>
+                <p>Email: {props.email}</p>
+                
+                <div className='ageInfo'>
+                    <button
+                        className='showHideAgeBtn'
+                        onClick={handleClick}
+                    >{ageConditionText} Age</button>
+                    {ageShown && <span className='age'>Age: {props.age}</span>}
+                </div>
+
+            </div>
         </div>
     )
 }
