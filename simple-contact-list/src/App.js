341889e5deca81638c328f760aba6a7ed12b65e5
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-03-11 15:30:04 +0200

    Added the main stucture of the Contact Card app

---
+++ simple-contact-list/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ContactCard from "./ContactCard";
+import "./general.css";
+
+function App() {
+    return (
+        <div>
+            <ContactCard />
+        </div>
+    )
+}
+
+export default App;
---
+++ simple-contact-list/src/ContactCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./contactCard.css";
+
+const ContactCard = () => {
+    return (
+        <div>   
+            
+        </div>
+    )
+}
+
+export default ContactCard
