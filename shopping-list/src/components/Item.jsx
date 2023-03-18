10c843ed034b345a7bec9f0208fdbe52d23eb939
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-03-18 17:40:37 +0200

    Added functionality and finished the app

--- shopping-list/src/components/Item.jsx
+++ shopping-list/src/components/Item.jsx
@@ -1,22 +1,46 @@
-import React from 'react'
+import { React, useState } from 'react'
 import "../css/item.css"
 
 const Item = (props) => {
+    const [name, setName] = useState(props.itemName);
+    const [bought, setBought] = useState(false);
+    const [itemsCount, setItemCount] = useState(1);
+
+    function changeStatus() {
+        setBought(prevStatus => !prevStatus);
+    }
+
+    function changeQuantity(event) {
+        const id = event.target.id;
+         
+        if (id === "increment") {
+            setItemCount(prevCount => prevCount + 1);
+            props.setCount(prevCount => prevCount + 1);
+        } else {
+            setItemCount(prevCount => prevCount > 1 ? prevCount - 1 : prevCount);
+            props.setCount(prevCount => itemsCount === 1 ? prevCount : prevCount - 1);
+        }
+    }
+
     return (
         <div className='itemContainer'>
             <input 
                 type="checkbox"  
                 className='itemCheck'
+                onChange={changeStatus}
             />
-            <p className='itemName'>Item 1</p>
+            <p className='itemName' 
+            style={{ textDecoration: bought ? "line-through" : "none"}}>{name}</p>
             <div className='quantityChanger'>
-                <i class="fa-solid fa-less-than"
-                
+                <i className="fa-solid fa-less-than"
+                    id="decrement"
+                    onClick={changeQuantity}
                 ></i>
-                <p className='itemQuantity'>1</p>
-                <i class="fa-solid fa-greater-than"
-                
-                    ></i>
+                <p className='itemQuantity'>{itemsCount}</p>
+                <i className="fa-solid fa-greater-than"
+                    id="increment"
+                    onClick={changeQuantity}                    
+                ></i>
             </div>
         </div>
     )
--- shopping-list/src/components/ShoppingListContainer.jsx
+++ shopping-list/src/components/ShoppingListContainer.jsx
@@ -1,23 +1,60 @@
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import React from 'react'
+import { React, useState } from 'react'
 import "../css/shoppingListContainer.css"
 import Item from './Item'
 
 const ShoppingListContainer = () => {
+    const [item, setItem] = useState("");
+    const [items, setItems] = useState([]);
+    const [totalCount, setTotalCount] = useState(0);
+    const itemsAsComponents = items.map((itm, idx) => {
+        return <Item
+                    key={idx}
+                    itemName={itm.itemName}
+                    setCount={setTotalCount}
+                />
+        
+    })
+   
+
+    function changeItem(event) {
+        setItem(event.target.value);
+    }
+
+    function addItem() {
+        if (item.trim() === "") {
+            return
+        }
+
+        setItems(prevItems => {
+            return [...prevItems, {itemName: item}];
+        })
+        setTotalCount(prevCount => prevCount + 1);
+        setItem(""); 
+    }
+
     return (
         <div className='listContainer'>
             <div className='addContainer'>
-                <input type="text" className='listInput' placeholder='Add an item...'/>
+                <input type="text" 
+                    className='listInput'
+                    placeholder='Add an item...'
+                    onChange={changeItem}
+                    value={item}
+                />
                 <div className='buttonDiv'>
                     <button
                         className='listAddBtn'
-                    ><i class="fa-solid fa-plus"></i></button>
+                        onClick={addItem}
+                    ><i className="fa-solid fa-plus"></i></button>
                 </div>
             </div>
 
            <div className='itemsList'>
-                <Item />
+                {itemsAsComponents}
            </div>
+
+           <div className='totalCount'>Total items: {totalCount}</div>
         </div>
     )
 }
