49719da6513f499c3b6c87546798f3b3d415d8c3
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-03-27 20:44:35 +0300

    Made the Navbar responsive

--- react-responsive-website/src/components/Navbar.jsx
+++ react-responsive-website/src/components/Navbar.jsx
@@ -1,24 +1,41 @@
-import React from 'react'
+import { React, useState } from 'react'
 import "../css/Navbar.css"
+import DropMenu from './DropMenu';
 
 const Navbar = () => {
+    const [clicked, setClicked] = useState(false);
+
+    function changeClicked() {
+        setClicked(prev => !prev);
+    }
+
     return (
-        <div className='navbarContainer'>
-            <img src="./logo.png" alt=""  className='logoImage'/>
-            <div className='navMenuContainer'>
+        <div>
+            <div className='navbarContainer'>
+                <img src="./logo.png" alt=""  className='logoImage'/>
+                <div className='navMenuContainer'>
+                    <button
+                        className='homeBtn'
+                    >Home</button>
+                    <button
+                        className='servicesBtn'
+                    >Services</button>
+                    <button
+                        className='productsBtn'
+                    >Products</button>
+                </div>
                 <button
-                    className='homeBtn'
-                >Home</button>
-                <button
-                    className='servicesBtn'
-                >Services</button>
-                <button
-                    className='productsBtn'
-                >Products</button>
+                    className='signUpBtn'
+                >Sign Up</button>
+                <div 
+                    onClick={changeClicked}
+                    className="burgerMenu"
+                >
+                    {clicked ?  <i className="fa-solid fa-x"></i> : <i className="fa-solid fa-bars"></i>}
+                </div>
             </div>
-            <button
-                className='signUpBtn'
-            >Sign Up</button>
+
+            {clicked && <DropMenu clicked={clicked}/>}
         </div>
     )
 }
