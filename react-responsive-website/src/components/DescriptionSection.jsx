6975e9d360d31a003d9520eb7d37a5aed2708bb7
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-03-28 20:27:42 +0300

    Made the title section responsive

---
+++ react-responsive-website/src/components/DescriptionSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../css/DescriptionSection.css"
+
+const DescriptionSection = () => {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default DescriptionSection
