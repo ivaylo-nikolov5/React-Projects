e9990c2973f48cf49c01cfdb3b7c9fd5ca8f96a1
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-03-28 18:53:32 +0300

    Created the style for the title section for pc

---
+++ react-responsive-website/src/components/TitleSection.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import "../css/TitleSectionContainer.css";
+import video from "../videos/bgVideo.mp4"
+
+
+
+const TitleSection = () => {
+
+    return (
+        <div className='titleSectionContainer'>
+            <video 
+                className='bgVideo'
+                autoPlay muted loop
+            >
+                <source src={video} type="video/mp4" />
+            </video>
+
+            <div className='titleContainer'>
+                <h1 className='title'><span className='name'>FTPC</span> Computer Service Ltd.</h1>
+                <p className='slogan'>Computer problems? We've got the solution!</p>
+                <div className='titleButtons'>
+                    <button className='servicesTitleBtn'>Services</button>
+                    <button className='productsTitleBtn'>Products</button>
+                </div>
+                <p className='descriptionRedirecterText'>MORE INFORMATION ABOUT US</p>
+                <div className='descriptionRedicrecter'>
+                    <i class="fa-solid fa-arrow-down"></i>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TitleSection
--- react-responsive-website/src/pages/MainPage.jsx
+++ react-responsive-website/src/pages/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from "../components/Navbar.jsx";
+import TitleSection from '../components/TitleSection.jsx';
 
 const MainPage = () => {
     return (
         <div>
             <Navbar />
+            <TitleSection />
         </div>
     )
 }
