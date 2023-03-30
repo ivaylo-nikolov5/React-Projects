b30fbcd3e5392dc51959007dc3eb5c20e8b9f44e
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-03-30 13:00:07 +0300

    Added section delimiter and made it responsive

---
+++ react-responsive-website/src/components/smallComponents/SectionDelimiter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SectionDelimiter = () => {
+    return (
+        <div className='sectionDelimiter'>
+            <hr className='leftLine'/>
+            <i class="fa-solid fa-microchip"></i>
+            <hr className='rightLine'/>
+        </div>
+    )
+}
+
+export default SectionDelimiter
--- react-responsive-website/src/pages/MainPage.jsx
+++ react-responsive-website/src/pages/MainPage.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import "../css/App.css";
 import DescriptionSection from '../components/DescriptionSection.jsx';
 import Navbar from "../components/Navbar.jsx";
 import TitleSection from '../components/TitleSection.jsx';
+import SectionDelimiter from '../components/smallComponents/SectionDelimiter';
 
 const MainPage = () => {
     return (
         <div>
             <Navbar />
             <TitleSection />
-            <hr />
+            
+            <SectionDelimiter />
+
             <DescriptionSection />
         </div>
     )
