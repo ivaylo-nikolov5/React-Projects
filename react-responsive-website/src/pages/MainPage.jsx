bd0109c32f767ce8c5aab2d509c40f89214d03aa
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-03-31 21:44:19 +0300

    Fixed a bug with the anchor

--- react-responsive-website/src/pages/MainPage.jsx
+++ react-responsive-website/src/pages/MainPage.jsx
@@ -14,6 +14,10 @@ const MainPage = () => {
             
             <SectionDelimiter />
 
+            <div className='toScroll'>
+
+            </div>
+
             <DescriptionSection />
 
             <SectionDelimiter/>
