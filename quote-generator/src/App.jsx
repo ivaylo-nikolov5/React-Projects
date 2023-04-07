4d21625e231cbe0996baa2c9c172e1eef2d3c6e7
Ivaylo <www.ivailo135@gmail.com>
2023-04-07 16:55:54 +0300

    Finished the app

--- quote-generator/src/App.jsx
+++ quote-generator/src/App.jsx
@@ -1,9 +1,38 @@
+import {React, useState, useEffect} from 'react';
 import './App.css';
 
+
 function App() {
+    const [quoteText, setQuoteText] = useState("");
+    const [state, setState] = useState(false);
+
+    useEffect(() => {
+        const apiUrl = 'https://api.adviceslip.com/advice';
+
+        fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            setQuoteText(data.slip.advice)
+        })
+        .catch(error => {
+            console.error('Error fetching quote:', error);
+        });
+    }, [state])
+
+    function generateQuote() {
+        setState(prevState => !prevState)
+    }
+
     return ( 
         <div className = "App" >
-
+                <img src="./city.jpg" alt=""  className='backgroundImage'/>
+                <div className='quoteContainer'>
+                    <h2 className='quoteText'>{quoteText}</h2>
+                    <button 
+                        className='generateQuoteBtn'
+                        onClick={generateQuote}
+                    >New Quote</button>
+                </div>
         </div>
     );
 }
